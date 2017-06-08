cd1c5ced88ba9f5b05d3c98096686eb5296832e0
Fabian Murariu <murariu.fabian@gmail.com>
2017-06-08 17:41:31 +0100

    changed gcloud submit

--- 02_apply_categories_extract_text.sh
+++ 02_apply_categories_extract_text.sh
@@ -2,7 +2,7 @@
 CLUSTER_NAME="cluster-1"
 gcloud dataproc jobs submit spark --cluster ${CLUSTER_NAME} \
   --class com.bytes32.prenn.PreNNProcessor \
-  --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar \
+  --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar -- \
   -r gs://websites-classify/raw \
   -w gs://websites-classify/websites-categories-clean \
   -t gs://websites-classify/parquet-text \
--- 03_transform_to_features.sh
+++ 03_transform_to_features.sh
@@ -2,7 +2,7 @@
 CLUSTER_NAME="cluster-1"
 gcloud dataproc jobs submit spark --cluster ${CLUSTER_NAME} \
   --class com.bytes32.prenn.PreNNTokenizer \
-  --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar \
+  --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar -- \
   -w gs://websites-classify/websites-categories-clean \
   -g gs://websites-classify/glove.6B.50d.txt.gz \
   -o gs://websites-classify/websites-features
