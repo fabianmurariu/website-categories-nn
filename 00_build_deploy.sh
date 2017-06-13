29df26632e0a2ccd681ca60753ae3681b555feb3
Fabian Murariu <murariu.fabian@gmail.com>
2017-06-13 15:45:34 +0100

    moving to master

--- 00_build_deploy.sh
+++ 00_build_deploy.sh
@@ -2,5 +2,5 @@
 set -x
 set -e
 cd sparky
-sbt clean assembly && gsutil cp process-dmoz/target/scala-2.11/process-dmoz-assembly-0.0.1.jar gs://tripll-data/code-deploy/
 gsutil cp process-dmoz/src/main/resources/subcategories.jl gs://websites-classify/
+sbt clean assembly && gsutil cp process-dmoz/target/scala-2.11/process-dmoz-assembly-0.0.1.jar gs://tripll-data/code-deploy/
