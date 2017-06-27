ba3f6faaeffc5eb4a596c67b7f062bc324d39b18
Fabian Murariu <murariu.fabian@gmail.com>
2017-06-27 10:24:20 +0100

    refactor train and added some tests, moved to streaming the data into the NN

---
+++ nn/predict_cat/train.py
@@ -0,0 +1,170 @@
+from __future__ import print_function
+
+import itertools
+import json
+from glob import glob
+
+import numpy as np
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.layers import Dense, Input, Flatten, Dropout, BatchNormalization
+from keras.models import Model
+from keras.optimizers import Adam
+
+
+def grouper(iterable, chunk_size):
+    a = iter(iterable)
+    while True:
+        chunk = list(itertools.islice(a, chunk_size))
+        if len(chunk) > 0:
+            yield chunk
+        else:
+            break
+
+
+def read_file_lines(file_path, fn):
+    fh = open(file_path, 'r')
+    while True:
+        line = fh.readline()
+        if not line:
+            break
+        yield fn(line)
+
+
+def read_lines(fn, *file_paths):
+    return itertools.chain(*map(lambda fp: read_file_lines(fp, fn), file_paths))
+
+
+def flat_map(tss):
+    for ts in tss:
+        for t in ts:
+            yield t
+
+
+def read_lines_paths(fn, file_paths, infinite=False):
+    if infinite:
+        return flat_map((read_file_lines(fp, fn) for fp in itertools.cycle(file_paths)))
+    else:
+        return itertools.chain(*(read_file_lines(fp, fn) for fp in file_paths))
+
+
+def json_lines(path):
+    return read_lines(json.loads, *glob(path))
+
+
+def text_lines(path):
+    return read_lines(lambda line: line.strip(), *glob(path))
+
+
+def class_weights(path, labels):
+    lines = list(json_lines(path))
+    labels_idx = {label: idx for label, idx in zip(labels, range(len(labels)))}
+    weights = lines[0]['weights']
+    return {labels_idx[label]: weight for label, weight in weights.iteritems() if label in labels_idx}
+
+
+def data_point(line):
+    point = json.loads(line)
+    x1 = np.array(point['paddedWords'])
+    y1 = np.array(point['category'])
+    return x1, y1, hash(line)
+
+
+def create_batch(b):
+    x = np.array([x for x, _ in b])
+    y = np.array([y for _, y in b])
+    return x, y
+
+
+def get_data(features_path, batch_size):
+    # valid
+    ds1 = list(read_lines(data_point, features_path + '/valid.json'))
+    valid_ds = [create_batch(b) for b in grouper([(x, y) for x, y, _ in ds1], batch_size)]
+    valid_set = set(z for _, _, z in ds1)
+    print("got validation")
+    # train
+    ds2 = read_lines_paths(data_point, glob(features_path + '/part*'), True)
+    lazy_train_set = ((x, y) for x, y, z in ds2 if z not in valid_set)
+    train_ds = (create_batch(b) for b in grouper(lazy_train_set, batch_size))
+
+    return valid_ds, train_ds
+
+
+def build_model(embeddings_path, labels, max_nb_words, embedding_dim=50, max_seq_length=1000):
+    embedding_matrix = np.array([item['vector'] for item in json_lines(embeddings_path + '/*')])
+    num_words = min(max_nb_words, len(embedding_matrix))
+    # load pre-trained word embeddings into an Embedding layer
+    # note that we set trainable = False so as to keep the embeddings fixed
+    embedding_layer = Embedding(num_words,
+                                embedding_dim,
+                                weights=[embedding_matrix],
+                                input_length=max_seq_length,
+                                trainable=False)
+
+    # train a 1D convnet with global maxpooling
+    sequence_input = Input(shape=(max_seq_length,), dtype='int32')
+    embedded_sequences = embedding_layer(sequence_input)
+    x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(5)(x)
+    x = BatchNormalization()(x)
+    x = Conv1D(128, 5, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(5)(x)
+    x = BatchNormalization()(x)
+    x = Conv1D(128, 5, activation='relu')(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling1D(35)(x)
+    x = BatchNormalization()(x)
+    x = Flatten()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(.1)(x)
+    x = BatchNormalization()(x)
+    x = Dense(len(labels), activation='softmax')(x)
+
+    opt = Adam()
+    model = Model(sequence_input, x)
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['acc'])
+    return model
+
+
+def fit_model(model, valid_ds, train_ds, class_weights):
+    model.fit_generator(train_ds, validation_data=itertools.cycle(valid_ds), steps_per_epoch=500,
+                        validation_steps=len(valid_ds),
+                        epochs=90, class_weight=class_weights)
+
+
+def save_model(model, path):
+    model.save(path)
+
+
+if __name__ == "__main__":
+    from os.path import expanduser
+
+    home = expanduser("~")
+    BASE_DIR = home + '/ml-work'
+    MAX_SEQUENCE_LENGTH = 1000
+    MAX_NB_WORDS = 20000
+    EMBEDDING_DIM = 50
+    batch_size = 16
+    # first, build index mapping words in the embeddings set
+    # to their embedding vector
+    pre_nn_output = BASE_DIR + '/dmoz/website-features-2'
+    embeddings_path = pre_nn_output + '/embeddings'
+    labels_path = pre_nn_output + '/labels'
+    features_path = pre_nn_output + '/features'
+    class_weights_path = pre_nn_output + '/class-weights'
+    # do stuff
+    labels = list(text_lines(labels_path + '/*'))
+    print("labels count %s" % len(labels))
+    class_weights = class_weights(class_weights_path + '/*', labels)
+    print("loading data")
+    valid_ds, train_ds = get_data(features_path, batch_size)
+    print("building model")
+    model = build_model(embeddings_path, labels, MAX_NB_WORDS, EMBEDDING_DIM, MAX_SEQUENCE_LENGTH)
+    print("training...")
+    fit_model(model, valid_ds, train_ds, class_weights)
+    print("done training saving model")
+    save_model(model, BASE_DIR + '/dmoz/model-keras')
---
+++ nn/test_nn.py
@@ -0,0 +1,36 @@
+from predict_cat import train
+import numpy as np
+
+
+def test_group_iterables_in_chunks():
+    actual = list(train.grouper([1, 2, 3, 4, 5, 6, 7, 8], 3))
+    expected = [[1, 2, 3], [4, 5, 6], [7, 8]]
+    assert actual == expected
+
+
+def test_expand_path_to_json_lines():
+    actual = list(train.json_lines('test_resources/part*'))
+    assert actual == [{'a': 1}, {'b': 2}, {'c': 3}, {'d': 4}]
+
+
+def test_expand_path_to_text_lines():
+    actual = list(train.text_lines('test_resources/part*'))
+    assert actual == ["{\"a\": 1}", "{\"b\": 2}", "{\"c\": 3}", "{\"d\": 4}"]
+
+
+def test_load_class_weights():
+    actual = train.class_weights('test_resources/class_weights.jl', ["bikes", "fashion", "health", "property"])
+    assert actual == {0: 27.924082140634724, 1: 8.422297297297296, 2: 3.32227733767676, 3: 3.5400757336699273}
+
+
+def test_data_point():
+    actual = train.data_point("""{"paddedWords":[3540,14221,3540,8227,17329,5953,17826],"category":[1,0]}""")
+    assert np.array_equal(actual[0], np.array([3540, 14221, 3540, 8227, 17329, 5953, 17826]))
+    assert np.array_equal(actual[1], np.array([1, 0]))
+    assert actual[2] == 2279133941201292847
+
+
+def test_create_batch():
+    x_actual, y_actual = train.create_batch([(1, 2), (3, 4)])
+    assert np.array_equal(x_actual, np.array([1, 3]))
+    assert np.array_equal(y_actual, np.array([2, 4]))
