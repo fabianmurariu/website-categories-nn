a409baea7588ba2108d0d6d4b190d1f8d2d5e36d
Fabian Murariu <murariu.fabian@gmail.com>
2017-12-31 09:31:12 +0000

    Changed the paths for 3 categories predict

--- 02_1_top3_categories_extract_text.sh
+++ 02_1_top3_categories_extract_text.sh
@@ -4,5 +4,5 @@ gcloud dataproc jobs submit spark --cluster ${CLUSTER_NAME} \
   --class com.bytes32.v2.PreNNProcessor \
   --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar -- \
   -r gs://websites-classify/raw \
-  -w gs://websites-classify/websites-categories-clean \
-  -t gs://websites-classify/parquet-text
+  -w gs://websites-classify/websites-categories-clean-top3 \
+  -t gs://websites-classify/parquet-text-top3
