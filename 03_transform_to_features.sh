0d3875c242479fe5920746cf6183184ba3f0d40b
Fabian Murariu <murariu.fabian@gmail.com>
2017-12-31 11:51:09 +0000

    rename scripts to output top3 data

--- 03_transform_to_features.sh
+++ 03_transform_to_features.sh
@@ -3,6 +3,6 @@ CLUSTER_NAME="cluster-1"
 gcloud dataproc jobs submit spark --cluster ${CLUSTER_NAME} \
   --class com.bytes32.prenn.PreNNTokenizer \
   --jar gs://tripll-data/code-deploy/process-dmoz-assembly-0.0.1.jar -- \
-  -w gs://websites-classify/websites-categories-clean \
+  -w gs://websites-classify/websites-categories-clean-top3 \
   -g gs://websites-classify/glove.6B.50d.txt.gz \
-  -o gs://websites-classify/websites-features
+  -o gs://websites-classify/websites-features-top3
